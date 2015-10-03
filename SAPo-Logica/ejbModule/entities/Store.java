dfef917bc9efa81b50a7cc934a5e15971c3499e8
malayon <malayonoyarbide@gmail.com>
2015-10-03 19:51:35 -0300

    Se corrige error en Store.java

--- SAPo-Logica/ejbModule/entities/Store.java
+++ SAPo-Logica/ejbModule/entities/Store.java
@@ -13,96 +13,88 @@ public class Store implements Serializable {
 	
 	@Id	
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String STORE_ID;	
+	private int identifier;	
 
-    private String STORE_NAME;
+    private String name;
 	
-    private String STORE_ADDRESS;
+    private String address;
 	
-    private String STORE_TELEPHONE;
+    private String telephone;
 	
-    private String STORE_CITY;
+    private String city;
 
     @OneToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="codigo")
-    private Usuario STORE_OWNER;
+    private Usuario owner;
     
     // LISTA DE LOS USUARIOS A LOS CUALES SE LES COMPATIO EL ALMACEN (EL DIENIO NO ESTA ACA)
     @OneToMany
-    private List<Usuario> STORE_USERS;
+    private List<Usuario> users;
     
    
     public Store() {
 		super();
 	}
-	public Store(String name, String addr, String telph, String city, Usuario user) {
+	public Store(String name, String addr, String tel, String city, Usuario user) {
 		super();
-		this.STORE_NAME = name;
-		this.STORE_ADDRESS = addr;
-		this.STORE_TELEPHONE = telph;
-		this.STORE_CITY = city;
-		this.STORE_OWNER = user;
-		this.STORE_USERS = new java.util.LinkedList<Usuario>();
+		this.name = name;
+		this.address = addr;
+		this.telephone = tel;
+		this.city = city;
+		this.owner = user;
+		this.users = new java.util.LinkedList<Usuario>();
 	}
 	
-	
-	public String getStoreName() {
-		return STORE_NAME;
-	}
-
-	public void setStoreName(String name) {
-		STORE_NAME = name;
-	}
-
-	public String getStoreAddress() {
-		return STORE_ADDRESS;
-	}
-
-	public void setStoreAddress(String addr) {
-		STORE_ADDRESS = addr;
-	}
-
-	public String getStoreTelephone() {
-		return STORE_TELEPHONE;
-	}
-
-	public void setStoreTelephone(String telph) {
-		STORE_TELEPHONE = telph;
-	}
-
-	public String getStoreCity() {
-		return STORE_CITY;
-	}
-
-	public void setStoreCity(String city) {
-		STORE_CITY = city;
-	}
-
-	public Usuario getStoreOwner() {
-		return STORE_OWNER;
-	}
-
-	public void setStoreOwner(Usuario owner) {
-		STORE_OWNER = owner;
-	}
-
-	public String getStoreID() {
-		return STORE_ID;
+	public int getIdentifier() {
+		return identifier;
 	}
 	
-	
-	public List<Usuario> getStoreUsers() {
-		return STORE_USERS;
+	public String getName() {
+		return name;
 	}
 	
-	public void setStoreUsers(List<Usuario> sTORE_USERS) {
-		STORE_USERS = sTORE_USERS;
-	}
-
-	public void addUserToStore( Usuario u ) {
-		this.STORE_USERS.add(u);
+	public void setName(String name) {
+		this.name = name;
 	}
 	
+	public String getAddress() {
+		return address;
+	}
 	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getTelephone() {
+		return telephone;
+	}
+	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public Usuario getOwner() {
+		return owner;
+	}
+	
+	public void setOwner(Usuario owner) {
+		this.owner = owner;
+	}
+	
+	public List<Usuario> getUsers() {
+		return users;
+	}
+	
+	public void setUsers(List<Usuario> users) {
+		this.users = users;
+	}
 	
 }
